dcae1ef71ad8abce5b7a5b9668fd5809b25f646f
unknown <tiagosousa0341@digitalcollege.internal>
2024-11-25 21:20:06 -0300

    update login page

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-
-function iterarMatriz2D() {
-    let matriz2D = [
-        ['l1 c1', 'l1 c2', 'l1 c3'],
-        ['l2 c1', 'l2 c2', 'l2 c3'],
-        ['l3 c1', 'l3 c2', 'l3 c3']
-    ];
-    
-    for(let i in matriz2D){
-        for(let k in matriz2D[i]){
-            console.log(`Linha "${Number(i)+1}", Coluna "${Number(k)+1}", Item "${matriz2D[i][k]}"`);
-        }
-    }
-}
-
 function preencherElementos() {
     const template = `
     <li class="flex flex-col flex-1 min-w-[256px] max-w-[30%]">
