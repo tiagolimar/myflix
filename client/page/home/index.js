d8316944b458a63b3c88f5b807b5540ee1808373
tiagolimar <tiagolima.r2@gmail.com>
2024-11-28 21:54:45 -0300

    test api movies

--- client/page/home/index.js
+++ client/page/home/index.js
@@ -1,5 +1,3 @@
-createMenu();
-
 function preencherElementos() {
     const template = `
     <li class="flex flex-col flex-1 min-w-[256px] max-w-[30%]">
@@ -12,7 +10,7 @@ function preencherElementos() {
     </li>        
     `
 
-    function preencherEm(id,template) {
+    function preencherEm(id, template) {
         const container = document.querySelector(`#${id} ul`);
         container.innerHTML = '';
 
@@ -25,3 +23,12 @@ function preencherElementos() {
 }
 
 preencherElementos();
+
+function testeAPI(){
+    const apiKey = '';
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${'robots'}&language=pt-BR`;
+
+    fetch(url).then(response=>response.json()).then(dados=>console.log(dados.results))
+}
+
+testeAPI();
